0a156ce5cf5d70e14731bca2839ca7c038a0b71d
benneee <benedictiknkeonye@gmail.com>
2020-01-29 05:07:26 +0100

    Tests for smaller snchronous functions

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,4 +1,6 @@
 const { calculateTip } = require("../src/math");
+const { fahrenheitToCelsius } = require("../src/math");
+const { celsiusToFahrenheit } = require("../src/math");
 
 test("Should calculate total with tip", () => {
   const total = calculateTip(10, 0.3);
@@ -8,4 +10,14 @@ test("Should calculate total with tip", () => {
 test("Should calculate total with default tip", () => {
   const total = calculateTip(10);
   expect(total).toBe(12.5);
+});
+
+test("Should convert 32F to 0C", () => {
+  const celsiusValue = fahrenheitToCelsius(32);
+  expect(celsiusValue).toBe(0);
+});
+
+test("Should convert 0C to 32F", () => {
+  const fahrValue = celsiusToFahrenheit(0);
+  expect(fahrValue).toBe(32);
 });
