b00f2cc6141033670d4d7963ef042ff5c2f5547c
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2020-02-03 22:21:04 +0100

    Tested the file upload and profile update endpoints

--- tests/user.test.js
+++ tests/user.test.js
@@ -120,3 +120,37 @@ test("Should not delete account for unauthenticated user", async () => {
 });
 
 // 401: "unauthorised"
+
+test("Should upload avatar image", async () => {
+  await request(app)
+    .post("/users/me/avatar")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .attach("avatar", "tests/fixtures/profile-pic.jpg")
+    .expect(200);
+
+  const user = await User.findById(userOneId);
+  expect(user.avatar).toEqual(expect.any(Buffer));
+});
+
+test("Should update valid user fields", async () => {
+  await request(app)
+    .patch("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send({
+      name: "Ola"
+    })
+    .expect(200);
+
+  const user = await User.findById(userOneId);
+  expect(user.name).toBe("Ola");
+});
+
+test("Should update valid user fields", async () => {
+  await request(app)
+    .patch("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send({
+      address: "Kano"
+    })
+    .expect(400);
+});
